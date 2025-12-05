444da209e63b97e42caa4cfa0ed51431320a424e
Retail Hugo <hugo.retail@etu.u-bordeaux.fr>
2025-12-05 02:35:47 +0100

    gitignore ajusts; linter config

--- web/eslint.config.mjs
+++ web/eslint.config.mjs
@@ -25,7 +25,7 @@ const eslintConfig = [
   },
   {
     rules: {
-      "@typescript-eslint/no-explicit-any": "error",
+      "@typescript-eslint/no-explicit-any": "off",
       "@typescript-eslint/no-require-imports": "error",
     },
   },
